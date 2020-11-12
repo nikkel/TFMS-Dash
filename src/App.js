4f728ce255be845de468b8d00a73f5238b4c6e4e
Ryan Clark <3845066+NikkelClark@users.noreply.github.com>
2020-11-12 14:40:58 +0000

    Cleanup for moving to another laptop

--- src/App.js
+++ src/App.js
@@ -1,58 +1,91 @@
 /** @format */
 
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import axios from 'axios';
 // //eslint-disable-next-line
 // import logo from './logo.svg';
-import logo from './sa-igloo-logo.gif';
+import logo from './sa-igloo-logo.png';
 import './App.css';
 import './AppStyle.css';
 
-function App() {
-  return (
-    <html>
-      <div class='wrapper'>
-        <div class='left-wrapper'>
-          <h1>Torn Faction Management System</h1>
-          <span id='welcome-text'>
-            Welcome,
-            <br />
-            Please login using your API key
-          </span>
-          <form>
-            <div class='form-outer'>
-              <label for='login-key'>Torn API Key</label>
-              <input
-                name='login-key'
-                type='text'
-                placeholder='Enter your Torn API Key...'
-              ></input>
-            </div>
-            <button type='submit'>Login.</button>
-          </form>
-        </div>
-        <div class='right-wrapper'>
-          <img src={logo} class='logo' alt='logo' />
-        </div>
-      </div>
-    </html>
+const tornURL = 'https://api.torn.com/user/?selections=profile&key=';
 
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+const App = () => {
+  const [token, setToken] = useState([]);
+
+  const getVerification = useCallback(async () => {
+    return await axios
+      .get(`${tornURL + token}`)
+      .then((response) => {
+        // If returns an error
+        if ('error' in response.data) {
+          const res = response.data['error'];
+          res['validToken'] = false;
+          return res;
+        }
+
+        // If return success
+        if (!('error' in response.data)) {
+          const res = response.data;
+          res['validToken'] = true;
+          return res;
+        }
+
+        return response.data;
+      })
+      .catch((err) => {
+        return;
+      });
+  });
+
+  useEffect(() => {});
+
+  const onTokenChange = useCallback((event) => {
+    setToken(event.target.value);
+    console.log(`Token Set: ${event.target.value}}`);
+  });
+
+  const onVerifyToken = useCallback(async (event) => {
+    event.preventDefault();
+    const response = await getVerification();
+    if (response['validToken'] === true) {
+      alert(
+        `Success, you're a part of ${response['faction']['faction_name']}!`
+      );
+    }
+    if (response['validToken'] === false) {
+      alert(`Invalid token! - {TORN: ${response['error']}}`);
+    }
+  });
+
+  return (
+    <div className='wrapper'>
+      <div className='left-wrapper'>
+        <h1>Torn Faction Management System</h1>
+        <span id='welcome-text'>
+          Welcome,
+          <br />
+          Please login using your API key
+        </span>
+        <form>
+          <div className='form-outer'>
+            <label htmlFor='login-key'>Torn API Key</label>
+            <input
+              type='text'
+              placeholder='Enter your Torn API Key...'
+              onChange={onTokenChange}
+            ></input>
+          </div>
+          <button type='button' onClick={onVerifyToken}>
+            Login.
+          </button>
+        </form>
+      </div>
+      <div className='right-wrapper'>
+        <img src={logo} className='logo' alt='logo' />
+      </div>
+    </div>
   );
-}
+};
 
 export default App;
