831f8fdcdcacc5854b3cdf4d22d70c136885ab63
nikkel <nikkel@users.noreply.github.com>
2022-02-03 22:22:19 +0000

    Deploy to GitHub pages

---
+++ precache-manifest.1935122a58fa2de8031dc3d6126d0a87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38ad6fb93f27274eedcb6c919effd1af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7f2c290f528ca331fdb",
+    "url": "/static/css/main.3fc8cdc0.chunk.css"
+  },
+  {
+    "revision": "fa5458eeb29790b74ba2",
+    "url": "/static/js/2.45bf61a5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.45bf61a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7f2c290f528ca331fdb",
+    "url": "/static/js/main.41c069d2.chunk.js"
+  },
+  {
+    "revision": "cf565f29a0b5d69a4ba8",
+    "url": "/static/js/runtime-main.b02c5971.js"
+  },
+  {
+    "revision": "1aa7368a47ce21886ffb080b7f74b596",
+    "url": "/static/media/logo.1aa7368a.png"
+  }
+]);
